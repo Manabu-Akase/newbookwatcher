777b9da777f8f19f39e2634de2b107b28e80d15f
ManabuAkase <doubt.m.39.knight@gmail.com>
2025-08-19 17:16:56 +0900

    中間テーブルに挿入するためのメソッド(insertBookAuthorCrossRef)追加

--- app/src/main/java/com/example/newbookwatcher/BookDao.java
+++ app/src/main/java/com/example/newbookwatcher/BookDao.java
@@ -9,34 +9,28 @@ import java.util.List;
 
 @Dao
 public interface BookDao {
-
     @Insert
     long insertBook(Book book);
+    @Insert
+    void insertBookAuthorsCrossRef(BookAuthorsCrossRef ref);
     @Update
     void update(Book book);
-
     @Delete
     void delete(Book book);
-
     @Query("SELECT * FROM books ORDER BY added_date DESC")
     List<Book> getAllBooks();
-
     @Query("SELECT * FROM books WHERE title LIKE '%' || :keyword || '%'")
     List<Book> searchBookByTitle(String keyword);
-
     @Query("SELECT * FROM books WHERE isFavorite = 1 ")
     List<Book> getFavoriteBooks();
-
     @Query("UPDATE books SET isFavorite = :isFavorite WHERE bookId = :bookId")
     void updateFavorite(int bookId, boolean isFavorite);
     //お気に入りの本を取得するクエリ
     @Transaction
     @Query("SELECT * FROM books WHERE isFavorite = 1 ")
     List<BookWithAuthors>getFavoriteBooksWithAuthors();
-
     //著者付きの検索メソッド
     @Transaction
     @Query("SELECT * FROM books WHERE title LIKE '%'||:keyword|| '%'")
     List<BookWithAuthors>searchBookWithAuthorsByTitle(String keyword);
-
 }
