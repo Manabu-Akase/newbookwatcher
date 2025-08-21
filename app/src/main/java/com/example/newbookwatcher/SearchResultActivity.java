cfede3facdce31eb48266c6c9ac14dac9a65fbdc
ManabuAkase <doubt.m.39.knight@gmail.com>
2025-08-21 15:31:13 +0900

    お気に入り状態を保存するスレッドをアイコンタップ時に行うよう修正

--- app/src/main/java/com/example/newbookwatcher/SearchResultActivity.java
+++ app/src/main/java/com/example/newbookwatcher/SearchResultActivity.java
@@ -1,5 +1,4 @@
 package com.example.newbookwatcher;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,7 +6,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -84,7 +82,7 @@ public class SearchResultActivity extends AppCompatActivity {
             }
         }).start();
 
-        //テスト用の本のデータをデータベースに登録する処理
+        //テスト用の本のデータをデータベースに登録する
         new Thread(new Runnable() {
             @Override
             public void run() {
