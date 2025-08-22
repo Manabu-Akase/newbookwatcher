76581b74014c474401ca07e254245a7be2e5ad75
ManabuAkase <doubt.m.39.knight@gmail.com>
2025-08-22 17:00:04 +0900

    リスナー、変数が重複定義されていたため削除

--- app/src/main/java/com/example/newbookwatcher/MenuActivity.java
+++ app/src/main/java/com/example/newbookwatcher/MenuActivity.java
@@ -15,46 +15,32 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-
         //検索欄にタイトルを入れて検索ボタンを押すと検索結果一覧画面で本を表示する処理(APIは未実装、画面遷移のみ)
-        Button seaerchButton = findViewById(R.id.SearchButton);
+        Button searchButton = findViewById(R.id.SearchButton);
         EditText searchBox = findViewById(R.id.editTextSearch);
 
-        seaerchButton.setOnClickListener(new View.OnClickListener() {
+        searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String keyword = searchBox.getText().toString().trim();
+                final String keyword = searchBox.getText().toString().trim();//入力されたキーワードの取得
 
-                //検索欄にタイトルを入れる＋検索ボタンを押した時の処理→入力したキーワードで本を探す、結果を画面のリストに表示する。
-                Button searchButton = findViewById(R.id.SearchButton);
-                EditText searchBox = findViewById(R.id.editTextSearch);
+                //テキストボックスが空白時の判定 → .isEmpty()の否定がテキストに入力されている状態
+                if (!keyword.isEmpty()) {
+                    //入力されたキーワードからタイトルを取得して検索結果画面に遷移
+                    Intent intent = new Intent(MenuActivity.this,SearchResultActivity.class);
+                    //キーワードをインテント先の画面に渡す
+                    intent.putExtra("keyword",keyword);
+                    startActivity(intent);
 
-                searchButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                        //入力されたキーワードの取得
-                        final String keyword = searchBox.getText().toString().trim();
-
-                        //入力されたキーワードからタイトルを取得して画面遷移(intent)
-                        //テキストボックスが空白時の判定 → .isEmpty()の否定がなにかしら入力されている状態！
-                        if (!keyword.isEmpty()) {
-                            Intent intent = new Intent(MenuActivity.this,SearchResultActivity.class);
-                            intent.putExtra("keyword",keyword);
-                            startActivity(intent);
-
-                            //エラー文を出す
-                        } else {searchBox.setError("検索する本のタイトルを入力してください");
-
-
-                        }
-                    }
-                });
+                    //テキストボックスが空の時エラー文を出す
+                } else {
+                    searchBox.setError("検索する本のタイトルを入力してください");
+                }
             }
         });
 
         //お気に入り一覧画面に遷移するボタンの処理
-        Button favoriteListButton =findViewById(R.id.favoriteListButton);
+        Button favoriteListButton = findViewById(R.id.favoriteListButton);
         favoriteListButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
