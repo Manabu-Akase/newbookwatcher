2be08cc08d1b6547bfa6aaba56a45966f9ccdfb4
ManabuAkase <doubt.m.39.knight@gmail.com>
2025-09-10 17:12:27 +0900

    [add]楽天APIから受け取ったItemフィールドを格納する中間テーブルを作成

---
+++ app/src/main/java/com/example/newbookwatcher/RakutenBookWrappr.java
@@ -0,0 +1,9 @@
+package com.example.newbookwatcher;
+
+import com.google.gson.annotations.SerializedName;
+
+class RakutenBookWrapper {
+    @SerializedName("Item")
+    public RakutenItem item;
+
+}
