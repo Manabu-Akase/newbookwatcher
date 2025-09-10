40919039def3aabfa71daf2c2cb2f02be40e3f72
ManabuAkase <doubt.m.39.knight@gmail.com>
2025-09-10 17:04:56 +0900

    [add]楽天APIから情報を受け取るデータを入れるクラスを作成。

---
+++ app/src/main/java/com/example/newbookwatcher/RakutenItem.java
@@ -0,0 +1,15 @@
+package com.example.newbookwatcher;
+import com.google.gson.annotations.SerializedName;
+
+public class RakutenItem {
+    @SerializedName("title")
+    public String title;
+    @SerializedName("author")
+    public String author;
+    @SerializedName("publisherName")
+    public String publisherName;
+    @SerializedName("salesDate")
+    public String salesDate ;
+    @SerializedName("ImageUrl")
+    public String ImageUrl;
+}
